ec5275384d6bd02dd0dc82415962150b09afc47c
Bruno Pereira <brunoleandropereira@hotmail.com>
2023-06-06 21:48:27 -0300

    T2: Add CustomError

--- trabalho2/alguma-sintatico/src/main/java/br/ufscar/dc/compiladores/alguma/sintatico/Principal.java
+++ trabalho2/alguma-sintatico/src/main/java/br/ufscar/dc/compiladores/alguma/sintatico/Principal.java
@@ -5,19 +5,21 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
+import java.io.File;
+import java.io.PrintWriter;
 
 public class Principal {
     public static void main(String args[]) throws IOException {
+        try(PrintWriter pw = new PrintWriter(new File(args[1]))) {
         CharStream cs = CharStreams.fromFileName(args[0]);
         AlgumaLexer lexer = new AlgumaLexer(cs);
-
-//        // Descomentar para depurar o Léxico
-//        Token t = null;
-//        while( (t = lexer.nextToken()).getType() != Token.EOF) {
-//            System.out.println("<" + AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
-//        }
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         AlgumaParser parser = new AlgumaParser(tokens);
+
+        MyCustomErrorListener mcel = new MyCustomErrorListener(pw);
+        parser.addErrorListener(mcel);
+
         parser.programa();
+    }
     }
 }
