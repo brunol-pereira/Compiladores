7f0e2e6406196fc9e9f0e871a7ebacfdbf71d45c
brunol-pereira <brunoleandropereira@hotmail.com>
2023-07-27 13:34:21 -0300

    Add comentários ao LA Semantico

--- trabalho3/LA-semantico/src/main/java/br/ufscar/dc/compiladores/la/semantico/LASemantico.java
+++ trabalho3/LA-semantico/src/main/java/br/ufscar/dc/compiladores/la/semantico/LASemantico.java
@@ -4,22 +4,26 @@ import br.ufscar.dc.compiladores.la.semantico.TabelaDeSimbolos.TipoLA;
 
 public class LASemantico extends LABaseVisitor<Void> {
 
-    Escopo escopos = new Escopo();
-    TabelaDeSimbolos tabelaDeSimbolos;
+    Escopo escopos = new Escopo(); // Gerenciar os escopos
+    TabelaDeSimbolos tabelaDeSimbolos; // Tabela de símbolos
 
     @Override
     public Void visitDeclaracao_local(LAParser.Declaracao_localContext ctx) {
+        // Trata declarações locais de variáveis
         for (var ctxIdentVariable : ctx.variavel().identificador()) {
             var varIdent = "";
             for (var ident : ctxIdentVariable.IDENT())
                 varIdent += ident.getText();
             var escopoAtual = escopos.obterEscopoAtual();
 
+            // Verifica se a variável já foi declarada anteriormente no mesmo escopo
             if (escopoAtual.existe(varIdent)) {
                 LASemanticoUtils.adicionarErroSemantico(ctxIdentVariable.IDENT(0).getSymbol(),
                         "identificador " + varIdent + " ja declarado anteriormente\n");
             } else {
+                // Obtém o tipo da variável
                 var varTipo = ctx.variavel().tipo().getText();
+                // Adiciona a variável à tabela de símbolos com seu tipo correspondente
                 if (varTipo.equals("inteiro")) {
                     escopoAtual.adicionar(varIdent, TabelaDeSimbolos.EstruturaLA.VARIAVEL, TipoLA.INTEIRO);
                 } else if (varTipo.equals("literal")) {
@@ -51,8 +55,10 @@ public class LASemantico extends LABaseVisitor<Void> {
 
     @Override
     public Void visitCmd(LAParser.CmdContext ctx) {
+        // Trata comandos, como leitura e atribuição de variáveis
         if (ctx.cmdLeia() != null) {
             var escopoAtual = escopos.obterEscopoAtual();
+            // Verifica se as variáveis a serem lidas foram declaradas anteriormente
             for (var ident : ctx.cmdLeia().identificador()) {
                 LASemanticoUtils.verificarTipo(escopoAtual, ident);
             }
@@ -60,6 +66,7 @@ public class LASemantico extends LABaseVisitor<Void> {
 
         if (ctx.cmdAtribuicao() != null) {
             var escopoAtual = escopos.obterEscopoAtual();
+            // Verifica se a atribuição é compatível com o tipo da variável à esquerda
             var leftValue = LASemanticoUtils.verificarTipo(escopoAtual,
                     ctx.cmdAtribuicao().identificador());
             var rightValue = LASemanticoUtils.verificarTipo(escopoAtual,
@@ -69,7 +76,6 @@ public class LASemantico extends LABaseVisitor<Void> {
                 LASemanticoUtils.adicionarErroSemantico(ctx.cmdAtribuicao().identificador().IDENT(0).getSymbol(),
                         "atribuicao nao compativel para " + ctx.cmdAtribuicao().identificador().getText() + "\n");
             }
-            
         }
 
         return super.visitCmd(ctx);
@@ -77,6 +83,7 @@ public class LASemantico extends LABaseVisitor<Void> {
 
     @Override
     public Void visitExp_aritmetica(LAParser.Exp_aritmeticaContext ctx){
+        // Trata expressões aritméticas e verifica se os tipos dos operandos são compatíveis
         var escopoAtual = escopos.obterEscopoAtual();
         LASemanticoUtils.verificarTipo(escopoAtual, ctx);
         return super.visitExp_aritmetica(ctx);
